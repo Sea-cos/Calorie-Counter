b1d954863227dbf77e62526271ebe708a09c04a3
Marcos Vinicius Santos da Silva <129910708+Sea-cos@users.noreply.github.com>
2024-01-18 15:03:09 -0300

    add: project final codes

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -19,7 +19,7 @@ function isInvalidInput(str) {
 
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
     const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
     <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
@@ -30,8 +30,8 @@ function addEntry() {
       id="${entryDropdown.value}-${entryNumber}-calories"
       placeholder="Calories"
     />`;
-    targetInputContainer.innerHTML += HTMLString;
-}
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+  }
 
 function calculateCalories(e) {
     e.preventDefault();
@@ -42,7 +42,6 @@ function calculateCalories(e) {
     const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
     const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
     const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
-
     const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
     const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
@@ -54,7 +53,19 @@ function calculateCalories(e) {
         return;
     }
 
+    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+    const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+    const surplusOrDeficit = remainingCalories >= 0 ? 'Surplus' : 'Deficit';
+    output.innerHTML =
+    `
+        <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+        <hr>
+        <p>${budgetCalories} Calories Budgeted</p>
+        <p>${consumedCalories} Calories Consumed</p>
+        <p>${exerciseCalories} Calories Burned</p>
+    `;
 
+    output.classList.remove('hide');
 }
 
 
@@ -75,4 +86,18 @@ function getCaloriesFromInputs(list) {
     return calories;
 }
 
+function clearForm() {
+    const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+
+    for (let i = 0; i < inputContainers.length; i++) {
+        inputContainers[i].innerHTML = '';
+    }
+
+    budgetNumberInput.value = '';
+    output.innerText = '';
+    output.classList.add('hide');
+}
+
 addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener("click", clearForm())
